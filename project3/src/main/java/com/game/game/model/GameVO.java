d5b980c6a39df2c8b39cc00c74231e746e423bfe
jung9 <jung9@118.37.17.144>
2021-11-25 18:22:14 +0900

    game 테이블 vo 랑 커넥션풀 - pro3으로 아이디 비번통일한거.

---
+++ project3/src/main/java/com/game/game/model/GameVO.java
@@ -0,0 +1,76 @@
+package com.game.game.model;
+
+import java.sql.Timestamp;
+
+public class GameVO {
+
+	private int g_no;
+	private String gname;
+	private int price;
+	private Timestamp gdate;
+	private String gtext;
+	private String notice;
+	private int number;
+	public GameVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public GameVO(int g_no, String gname, int price, Timestamp gdate, String gtext, String notice, int number) {
+		this.g_no = g_no;
+		this.gname = gname;
+		this.price = price;
+		this.gdate = gdate;
+		this.gtext = gtext;
+		this.notice = notice;
+		this.number = number;
+	}
+	public int getG_no() {
+		return g_no;
+	}
+	public void setG_no(int g_no) {
+		this.g_no = g_no;
+	}
+	public String getGname() {
+		return gname;
+	}
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Timestamp getGdate() {
+		return gdate;
+	}
+	public void setGdate(Timestamp gdate) {
+		this.gdate = gdate;
+	}
+	public String getGtext() {
+		return gtext;
+	}
+	public void setGtext(String gtext) {
+		this.gtext = gtext;
+	}
+	public String getNotice() {
+		return notice;
+	}
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	@Override
+	public String toString() {
+		return "GameVO [g_no=" + g_no + ", gname=" + gname + ", price=" + price + ", gdate=" + gdate + ", gtext="
+				+ gtext + ", notice=" + notice + ", number=" + number + "]";
+	}
+	
+	
+}
